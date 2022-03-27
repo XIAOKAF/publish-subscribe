8d321cccd5c2d68c9157783ac73355f06c113113
XIAOKAF <2021212196@stu.cqupt.edu.cn>
2022-03-27 16:53:25 +0800

    refactor:redis连接池参数配置

--- dao/initRedis.go
+++ dao/initRedis.go
@@ -1,18 +1,29 @@
 package dao
 
-import "github.com/go-redis/redis"
+import (
+	"github.com/go-redis/redis"
+	"log"
+	"time"
+)
 
 var redisDB *redis.Client
 
-func InitRedis() error {
+func InitRedis() {
 	redisDB = redis.NewClient(&redis.Options{
-		Addr:     "localhost:6379",
-		Password: "",
-		DB:       0,
+		Addr:         "localhost:6379",
+		Password:     "",
+		DB:           0,
+		PoolSize:     15, //连接池最大连接数
+		MinIdleConns: 5,  //闲置连接数量
+		//超时设置
+		DialTimeout:  5 * time.Second, //连接建立的超时时间
+		ReadTimeout:  3 * time.Second, //读超时，-1表示取消读超时
+		WriteTimeout: 3 * time.Second, //写超时
+		PoolTimeout:  4 * time.Second, //党所有连接都处在繁忙的状态时，客户端等待可用连接的最大时间
 	})
 	_, err := redisDB.Ping().Result()
 	if err != nil {
-		return err
+		log.Fatal(err)
 	}
-	return nil
+	defer redisDB.Close()
 }
