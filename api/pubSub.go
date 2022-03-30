2c172c3307083ffe6d5f7aecd62d83fccad751a2
XIAOKAF <2021212196@stu.cqupt.edu.cn>
2022-03-30 17:25:57 +0800

    feat:新增删除频道的接口

--- api/pubSub.go
+++ api/pubSub.go
@@ -20,3 +20,15 @@ func createPublisher(ctx *gin.Context) {
 	pubSub[publisher] = subs
 	tool.Success(ctx, 200, "你已经成功创建频道"+publisher)
 }
+
+//删除频道
+func deletePublisher(ctx *gin.Context) {
+	publisher := ctx.PostForm("publisher")
+	_, ok := pubSub[publisher]
+	if !ok {
+		tool.Failure(ctx, 200, "该频道不存在")
+		return
+	}
+	delete(pubSub, publisher)
+	tool.Success(ctx, 200, "您已成功删除频道"+publisher)
+}
--- api/router.go
+++ api/router.go
@@ -7,7 +7,7 @@ func InitEngine() {
 	publisherGroup := engine.Group("/publisher")
 	{
 		publisherGroup.POST("/createPublisher", createPublisher) //创建频道
-		//publisherGroup.POST("/deletePublisher", deletePublisher) //删除频道
+		publisherGroup.POST("/deletePublisher", deletePublisher) //删除频道
 		//publisherGroup.POST("/publish", publish)                 //发布消息
 	}
 	//engine.POST("/subscribe", subscribe) //订阅频道
