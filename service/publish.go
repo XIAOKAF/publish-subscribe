6a537f91f8e311e85bd5427b8f84ec6a3c60bbb3
XIAOKAF <2021212196@stu.cqupt.edu.cn>
2022-03-30 17:41:49 +0800

    refactor:后台显示频道发布信息

--- service/publish.go
+++ service/publish.go
@@ -1,12 +1,13 @@
 package service
 
 func Publish(publisher string, info string, pubSub map[string][]string) {
+	getInfo := make(map[string]string)
 	channel := make(chan string)
 	for _, value := range pubSub[publisher] {
 		go func() {
 			channel <- info
 		}()
-		value = <-channel
-		println(value)
+		getInfo[value] = <-channel
+		println(publisher + "给" + value + "发来了一条信息：" + getInfo[value])
 	}
 }
